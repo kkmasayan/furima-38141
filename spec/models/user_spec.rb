d0c32d6c7f478bc87d64bdd8b88b024b28077d41
kkmasayan <108529674+kkmasayan@users.noreply.github.com>
2022-09-25 20:28:05 +0900

    #4 ユーザー管理機能　修正１

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,9 +45,21 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordが半角英数字でないと登録できない' do
+      it 'passwordが数字のみでは登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+      it 'passwordが英字のみでは登録できない' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+      it 'passwordが全角文字を含むと登録できない' do
+        @user.password = 'ａ1234５'
+        @user.password_confirmation = 'ａ1234５'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
       end
