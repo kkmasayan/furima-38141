02e7c59fbc5c1d12cd2e552d7232fc460a4594e4
kkmasayan <108529674+kkmasayan@users.noreply.github.com>
2022-10-06 02:49:37 +0900

    #5 商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:user]
   def index
+
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit( :name, :description, :status_condition_id, :prefecture_id, :shipping_charges_id, :shipping_days_id, :shipping_price, :user_id, :category_id, :image).merge(user_id: current_user.id)
   end
 end
---
+++ spec/factories/status_conditions.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :status_condition do
+    
+  end
+end
