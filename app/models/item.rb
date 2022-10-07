465c3f0f9e05e4505eb98283cdf922d0570e7ba4
kkmasayan <108529674+kkmasayan@users.noreply.github.com>
2022-10-07 17:23:51 +0900

    #5 商品出品機能　修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :category
   belongs_to :shipping_charge
-  belongs_to :shipping_day, optional: true
+  belongs_to :shipping_date
 
   has_one_attached :image
 
