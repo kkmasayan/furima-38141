b7b45298ce8e6747daaa82577ce05257eb6851bd
kkmasayan <108529674+kkmasayan@users.noreply.github.com>
2022-10-07 19:36:18 +0900

    #5　商品出品機能　修正5

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     shipping_days_id { Faker::Number.between(from: 1, to: 3) }
     shipping_price { Faker::Number.between(from: 300, to: 9_999_999) }
     category_id { Faker::Number.between(from: 1, to: 10) }
-    association :user
+    association :user, strategy: :create
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
