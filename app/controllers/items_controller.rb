d7ea4b1665e9ca72dff6534afbc6aad2f077ad13
kkmasayan <108529674+kkmasayan@users.noreply.github.com>
2022-10-12 22:16:55 +0900

    #9 商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -37,6 +37,19 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if user_signed_in?
+      if @item.user_id == current_user.id
+        @item.destroy
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
+    else
+      redirect_to registration_new_path
     end
   end
 
